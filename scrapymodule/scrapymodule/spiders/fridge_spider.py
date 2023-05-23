22e912a3b2d1311990b2babeb4cad7a886e5aab6
Vitalii Brovdi <brovdi.vitaliy@gmail.com>
2023-05-23 12:23:18 +0300

    Initial commit

---
+++ scrapymodule/scrapymodule/spiders/fridge_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class FridgeSpider(scrapy.Spider):
+    name = 'fridge_spider'
+    start_urls = ['https://ek.ua/ua/list/149/']
+
+    def parse(self, response):
+        products = response.css('.list_form1')
+        for product in products:
+            model = product.css('.model-short-title a::title').get()
+            image_url = product.css('.model-short-img img::attr(src)').get()
+            shop = product.css('.model-short-shop span::text').get()
+            price = product.css('.model-short-price::text').get()
+            yield {
+                'model': model,
+                'image_url': image_url,
+                'shop': shop,
+                'price': price,
+            }
+
+        next_page = response.css('.paginator .active + a::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
